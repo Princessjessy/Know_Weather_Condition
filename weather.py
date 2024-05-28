d62fcf811b095033c0ba962477e4b9c73c6a9735
Princessjessy <ebubeprincess590@gmail.com>
2024-05-28 13:45:32 +0100

    project

---
+++ weather.py
@@ -0,0 +1,26 @@
+from dotenv import load_dotenv
+from pprint import pprint
+import requests
+import os
+
+load_dotenv()
+
+
+def know_current_weather(town="Ikeja Town"):
+
+    request_url = f'http://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={town}&units=imperial'
+
+    weather_data = requests.get(request_url).json()
+
+    return weather_data
+
+
+if __name__ == "__main__":
+    print('\n*** Know Current Weather Conditions ***\n')
+
+    town = input("\nPlease enter a town name: ")
+
+    weather_data = know_current_weather(town)
+
+    print("\n")
+    pprint(weather_data)
